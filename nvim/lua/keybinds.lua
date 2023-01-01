cba7fa7f2ace9657db755ee9a6d5f59600f43794
carnoldcoding <carnoldcoding@gmail.com>
2022-12-31 20:15:38 -0500

    more nvim

--- nvim/lua/keybinds.lua
+++ nvim/lua/keybinds.lua
@@ -1,6 +1,6 @@
 --Remap Function
-function map(mode, keys, action, opts)
-	local options = { noremap = true }
+function map(mode, keys, action, descr, opts)
+	local options = { noremap = true, desc = descr }
 	if opts then
 		options = vim.tbl_extend("force", options, opts)
 	end
@@ -8,13 +8,19 @@ function map(mode, keys, action, opts)
 end
 
 --Insert Mode
-map('i', 'jk', '<ESC>')
+map('i', 'jk', '<ESC>', 'Insert Mode')
 
 --Normal Mode
+map('n', '<leader>q', ':q<cr>', 'Quit')
+map('n', '<leader>Q', ':wq<cr>', 'Save & Quit')
+map('n', '<leader>w', ':w<cr>', 'Save')
+map('n', '<leader>x', ':bd<cr>', 'Closer Buffer')
+
 ----NvimTree Binds
-map('n', '<leader>e', ':NvimTreeToggle<CR>') --<CR> is Character Return a.k.a 'enter'
+map('n', '<leader>e', ':NvimTreeToggle<CR>', 'Toggle Tree') 
+
 ----Window Movement
-map('n', '<leader>h', '<C-w>h')
-map('n', '<leader>l', '<C-w>l')
-map('n', '<leader>j', '<C-w>j')
-map('n', '<leader>k', '<C-w>k')
+map('n', '<leader>h', '<C-w>h', 'Switch to Left Buffer')
+map('n', '<leader>l', '<C-w>l', 'Switch to Right Buffer')
+map('n', '<leader>j', '<C-w>j', 'Switch to Bottom Buffer')
+map('n', '<leader>k', '<C-w>k', 'Switch to Top Buffer')
--- nvim/plugin/whichkey.lua
+++ nvim/plugin/whichkey.lua
@@ -1,11 +1,1 @@
 require('which-key').setup{}
-
-local wk = require('which-key')
-local mappings = {
-	q = {':q<cr>', 'Quit'},
-	Q = {':wq<cr>', 'Save & Quit'},
-	w = {':w<cr>', 'Save'},
-	x = {':bd<cr>', 'Close Buffer'}
-}
-local opts = {prefix = '<leader>'}
-wk.register(mappings, opts)
